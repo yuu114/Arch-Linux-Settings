a32c3dfe2bbfe6cf44f0dbe6d60f6590ecc2058f
yuu114 <yuu114@protonmail.com>
2018-01-16 13:45:04 +0100

    rainbow dash theme

--- i3configV1/.bin/updateDotfiles.sh
+++ i3configV1/.bin/updateDotfiles.sh
@@ -7,7 +7,7 @@ cp ~/.config/i3/config ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/i3confi
 cp ~/.config/i3status/config ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/i3status
 cp ~/.Xresources ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
 cp ~/.Xdefaults ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
-cp ~/.bashrc ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
+# cp ~/.bashrc ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
 cp -r ~/.bin ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
 rm ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/.bin/myServer.sh
 # echo ""
