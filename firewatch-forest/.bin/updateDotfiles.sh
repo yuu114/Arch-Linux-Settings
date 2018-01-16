c7292f87ac1395bb4fe136a7c142b6a744ddfe00
yuu114 <yuu114@protonmail.com>
2018-01-16 13:49:48 +0100

    old folder seperated new theme in i3conf

---
+++ firewatch-forest/.bin/updateDotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "####################"
+echo "## copying files  ##"
+echo "####################"
+echo ""
+cp ~/.config/i3/config ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/i3config
+cp ~/.config/i3status/config ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/i3status
+cp ~/.Xresources ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
+cp ~/.Xdefaults ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
+# cp ~/.bashrc ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
+cp -r ~/.bin ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/
+rm ~/Dropbox/settings_linux/arch/dotfiles/i3configV1/.bin/myServer.sh
+# echo ""
+# echo "####################"
+# echo "## updating git   ##"
+# echo "####################"
+# echo ""
+# git add .
+# git commit -m "changed something I guess"
+echo ""
+echo "####################"
+echo "## done           ##"
+echo "####################"
+echo ""
