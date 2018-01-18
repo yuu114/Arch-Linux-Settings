7d21d1a1f194290838fbefcb8859bf5c2071818c
yuu114 <yuu114@protonmail.com>
2018-01-18 15:12:34 +0100

    added rainbow-dash theme

---
+++ i3config/.bin/myAutopdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script for updating system
+echo "##################################"
+echo "##                              ##"
+echo "##  Running pacman -Syyu        ##"
+echo "##                              ##"
+echo "##  Checking for Updates ..     ##"
+echo "##                              ##"
+echo "##################################"
+echo ""
+echo ""
+sudo pacman -Syyu
+echo ""
+echo "#################################"
+echo "             Done."
+echo ""
